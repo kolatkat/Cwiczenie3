b9e08e79223cb43f631cea3ac34926c9a88943a5
Kasia Kołata <katarzyna.kolata3@gmail.com>
2019-11-23 19:27:15 +0100

    Przesłano pliki na Github po poprawkach

--- Projekt3/Projekt3/Program.cs
+++ Projekt3/Projekt3/Program.cs
@@ -9,9 +9,13 @@ namespace Zadanie3
             Console.Write("Wprowadź dowolną liczbę całkowitą: ");
             string input = Console.ReadLine();
             Console.WriteLine();
-            if (int.TryParse(input, out integer)
+
+            int number;
+            if (int.TryParse(input, out number))
             {
                 Console.WriteLine("Wprowadzono liczbę całkowitą '" + input + "'");
+                Console.WriteLine("Brawo! Trzecie zadanie zrobione :)");
+                Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
             }
             else
             {
@@ -20,10 +24,11 @@ namespace Zadanie3
                     input = "(pusty string)";
                 }
                 Console.WriteLine("Wprowadzona wartośc nie jest liczbą całkowitą!" + Environment.NewLine + "Wprowadzono '" + input + "'");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
         }
-        Console.WriteLine();
-        Console.WriteLine("Brawo! Trzecie zadanie zrobione :)");
-        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
-        Console.ReadKey();
     }
 }
